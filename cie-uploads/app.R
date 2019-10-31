7c02482909fd6a294ce90385b0cef6d33ea59796
cie <dhuy228@aucklanduni.ac.nz>
2019-10-31 00:05:28 +0000

    Fixed #55

--- cie-uploads/app.R
+++ cie-uploads/app.R
@@ -9,6 +9,7 @@ library(tidyr)
 # library(reshape2)
 library(tidyverse)
 library(readxl)
+library(xlsx)
 library(dplyr)
 # library(plotly)
 library(DT)
@@ -16,6 +17,7 @@ library(tools)
 # library(shinyWidgets)
 data_dir <- "../data"
 backup_dir <- "../backup_data"
+#tags_dir <- "../ta"
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -38,29 +40,34 @@ ui <- fluidPage(
                                              ".xlsx",
                                              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")),
                         
-                        # Name
-                        textInput("saveName", "Save file name as", placeholder="Enter file name..."),
-                        
                         # Year
                         selectInput("saveYear", "Select year", choices = 2015:as.numeric(format(Sys.Date(),"%Y"))+1, selected = as.numeric(format(Sys.Date(),"%Y"))),
                         
                         # Type
-                        radioButtons("saveType", "Select type of file", choices = c("None", "SSO" = "From Rachel - ", "CRM" = "Original - ", "TAG" = "tags_selection")),
+                        radioButtons("saveType", "Select type of file", choices = c("None", "SSO" = "From Rachel - ", "CRM" = "Original - ", "TAG" = "tags_selection -")),
                         
                         # Save Button ----
-                        actionButton("save", "Save")
+                        actionButton("save", "Save"),
                         
+                        # Horizontal line ----
+                        tags$hr(),
+                        
+                        # Guide ----
+                        tags$em("For EXCEL files"),
+                        tags$li(tags$sub("SSO excel files must contain 'Student' Sheet")),
+                        tags$li(tags$sub("CRM excel files must contain 'contacts' Sheet")),
+                        tags$li(tags$sub("TAGS excel files must contain 'Tags' Sheet")),
+                        tags$br()
                 ),
                 
                 # Main panel for displaying outputs ----
                 mainPanel(
-                        #verbatimTextOutput("value"),
+                        # Name
+                        #textInput("saveName", "Save file name as", placeholder="Enter file name..."),
+                        verbatimTextOutput("saveFileName"),
                   
                         # Output: Data file ----
                         dataTableOutput("contents")
-                        
-                        
-                        
                 )
                 
         )
@@ -72,13 +79,14 @@ server <- function(input, output) {
         data <- reactive({
                 # Error messages
                 validate(
-                        need(input$saveName != "" && (grepl("From",input$saveName, fixed = TRUE) || grepl("Original",input$saveName, fixed =TRUE)), message="Please enter a valid filename"),
+                        #need(input$saveName != "" && (grepl("From",input$saveName, fixed = TRUE) || grepl("Original",input$saveName, fixed =TRUE)), message="Please enter a valid filename"),
                         #need(!file.exists(file.path("data", input$saveYear, input$saveName)), message = "File already exists"),
                         need(input$saveYear != "", message="Please select valid year"),
+                        #need(input$uploadFile$datapath != "", message="Please select files to upload"),
                         need(input$saveType != "None", message = "Please select file type")
                         
                 )
-                req(input$uploadFile, input$saveName, input$saveYear)
+                req(input$uploadFile, input$saveType, input$saveYear)
                 
                 # Basename
                 uploadPath <- input$uploadFile$datapath
@@ -91,18 +99,16 @@ server <- function(input, output) {
                 } else if (file_ext(uploadPath) == "xlsx") {
                         
                         # Import tags_selection.xlsx
-                        if (input$saveType == "tags_selection") {
+                        if (input$saveType == "tags_selection" && "Tags" %in% excel_sheets(uploadPath)) {
                                 df <- read_excel(uploadPath, sheet = "Tags")
                         }
                         # Import From.*xlsx
-                        else if ("Student" %in% excel_sheets(uploadPath) || input$saveType == "SSO") {
+                        else if ("Student" %in% excel_sheets(uploadPath) && input$saveType == "From Rachel - ") {
                                 df <- read_excel(uploadPath, sheet="Student", skip = 1)
                         }
-                       
-                        else {
+                        else if ("contacts" %in% excel_sheets(uploadPath) && input$saveType == "Original - ") {
                                 df  <- read_excel(uploadPath)
                         }
-
                 }
                 return(df)
         })
@@ -118,29 +124,77 @@ server <- function(input, output) {
                 return(df)
         })
         
+        saveName <- reactive({
+          req(input$uploadFile, input$saveType, input$saveYear)
+          validate(
+            need(input$saveType != "None", message="")
+          )
+          uploadPath <- basename(input$uploadFile$datapath)
+          
+          # Add time stamp
+          name <- paste0(input$saveType, input$saveYear, " ", Sys.time())
+          
+          # Add file extension
+          name <- paste(name, file_ext(uploadPath), sep =".")
+          
+          return(name)
+        })
+        
         #output$value <- renderPrint( {input$saveType})
+        output$saveFileName <- renderPrint({saveName()})
+        #output$saveFileName <- renderPrint(file.exists(file.path(data_dir,input$saveYear)))
 
         observeEvent(input$save, {
-                req(input$csvFile, input$saveName, input$saveYear)
-                uploadPath <- basename(input$uploadFile$datapath)
+                req(input$uploadFile, input$saveType, input$saveYear)
+                uploadPath <- basename(saveName())
                 
-                # Get basename
-                saveName <- basename(input$saveName)
-                
-                # Add time stamp
-                saveName <- paste(saveName, Sys.time())
-                
-                if (file_ext(uploadPath == "csv")) {
-                        
-                        write.csv(data(), file = file.path(data_dir, input$saveYear, saveName), row.names = FALSE, quote = TRUE)
-                        
-                } else if (file_ext(uploadPath == "xlsx")) {
-                        
-                        df  <- read_excel(uploadPath)
-                        
+                # create subdirectory by year if it doesn't exist
+                if (!file.exists(file.path(data_dir,input$saveYear))) {
+                  dir.create(file.path(data_dir, input$saveYear))
+                }
+                if (!file.exists(file.path(backup_dir, input$saveYear))) {
+                  dir.create(file.path(backup_dir, input$saveYear))
                 }
                 
+                # Copy overwrite previous data to backup directory
+                if (input$saveType == "tags_selection - ") {
+                  checkDir <- dir(file.path(data_dir, "tags"), pattern = paste0(input$saveType, ".*"), full.names = TRUE)
+                  checkDir2 <- file.path(backup_dir, "tags")
+                } else {
+                  checkDir <- dir(file.path(data_dir, input$saveYear), pattern = paste0(input$saveType, ".*"), full.names = TRUE)
+                  checkDir2 <- file.path(backup_dir, input$saveYear)
+                }
+                file.copy(checkDir, checkDir2, recursive = TRUE)
+                
+                #remove previous data from data directories
+                file.remove(checkDir)
+                
+                # save uploaded files to data and backup data directories
+                if (file_ext(uploadPath) == "csv") {
+                        # Save .csv data files
+                        write.csv(data(), file = file.path(data_dir, input$saveYear, saveName()), row.names = FALSE, quote = TRUE)
+                  
+                } else if (file_ext(uploadPath) == "xlsx") {
+                  
+                        if (input$saveType == "tags_selection -") {
+                          
+                          # Save tag files
+                          write.xlsx2(data(), file=file.path(data_dir, "tags", saveName()))
+                          
+                        } else {
+                          
+                          # Save .xlsx data files
+                          write.xlsx2(data(), file=file.path(data_dir, input$saveYear, saveName()))
+                          
+                        }
+                }
+                
+                # run the data management script functions
+                
+                
+                # print output messsage
         })
+        
 }
 
 shinyApp(ui, server)
